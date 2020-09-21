8d219934dd68a01bcab1da64c7b497e783f8ead2
AlexGrinch <grinchuk.alexey@gmail.com>
2020-09-21 11:45:19 -0700

    added scripts

---
+++ get_code.sh
@@ -0,0 +1,6 @@
+function gdrive_download () {
+  CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
+  rm -rf /tmp/cookies.txt
+}
+gdrive_download 10r0TynTN0aBk-tWF9Jfv56gMz6wIlX2k data.zip
